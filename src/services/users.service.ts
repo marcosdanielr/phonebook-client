4d78e2802b45490850baa0b50ffbc57ad224a42c
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-01 06:12:49 -0300

    feat: get current user (service)

---
+++ src/services/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserModel } from '../models/user.model';
+import { lastValueFrom } from 'rxjs';
+
+interface UserResponse {
+  user: UserModel;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  constructor(private http: HttpClient) {}
+
+  async getCurrentUser(): Promise<UserModel> {
+    try {
+      const { user } = await lastValueFrom(
+        this.http.get<UserResponse>('/api/auth/current_user', {
+          responseType: 'json',
+        })
+      );
+
+      return user;
+    } catch (error) {
+      console.log({ error });
+      return error as any;
+    }
+  }
+}
