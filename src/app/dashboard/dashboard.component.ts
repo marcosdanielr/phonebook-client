07d5dca03272c714a950450809732ead4d416f61
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-01 04:17:31 -0300

    feat: header

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   presets: [require("@spartan-ng/ui-core/hlm-tailwind-preset")],
   content: ["./src/**/*.{html,ts}", "./src/libs/ui/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "slate-900": "#0F172A",
+      },
+    },
   },
   plugins: [],
 };
