7c180170fabe408e8e3d01eaf6145596290a37ed
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-01 05:16:52 -0300

    feat: auth controller

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,17 +1,14 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
 import { HlmButtonDirective } from '../../libs/ui/ui-button-helm/src/lib/hlm-button.directive';
 import { HlmInputDirective } from '../../libs/ui/ui-input-helm/src/lib/hlm-input.directive';
 import { HlmIconComponent } from '../../libs/ui/ui-icon-helm/src/lib/hlm-icon.component';
 import { HlmLabelDirective } from '../../libs/ui/ui-label-helm/src/lib/hlm-label.directive';
-import { AuthenticateRequestModelView } from '../../models-views/auth.model-view';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 import { provideIcons } from '@ng-icons/core';
 import { lucideEye, lucideEyeOff } from '@ng-icons/lucide';
-import { RoutesEnum } from '../../constants/routes.enum';
+import { AuthController } from '../../controllers/auth.controller';
 
 @Component({
   selector: 'app-sign-in',
@@ -29,42 +26,10 @@ import { RoutesEnum } from '../../constants/routes.enum';
   ],
   providers: [provideIcons({ lucideEye, lucideEyeOff })],
 })
-export class SignInComponent {
+export class SignInComponent extends AuthController {
   public isShowingPassword = false;
-  public loginForm = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  });
-  public isLoading = false;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {}
 
   handleTogglePasswordVisibility(): void {
     this.isShowingPassword = !this.isShowingPassword;
-  }
-
-  async handleLogin(event: Event): Promise<void> {
-    event.preventDefault();
-
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    this.isLoading = true;
-
-    try {
-      await this.authService.login(
-        this.loginForm.value as AuthenticateRequestModelView
-      );
-      this.router.navigate([RoutesEnum.DASHBOARD]);
-    } catch (error) {
-      alert('Credenciais inválidas!');
-    } finally {
-      this.isLoading = false;
-    }
   }
 }
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,49 @@
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { AuthenticateRequestModelView } from '../models-views/auth.model-view';
+import { RoutesEnum } from '../constants/routes.enum';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthController {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  public loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+  public isLoading = false;
+
+  async login(event: Event): Promise<void> {
+    event.preventDefault();
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    try {
+      await this.authService.login(
+        this.loginForm.value as AuthenticateRequestModelView
+      );
+      this.router.navigate([RoutesEnum.DASHBOARD]);
+    } catch (error) {
+      alert('Credenciais inválidas!');
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate([RoutesEnum.LOGIN]);
+  }
+}
