79f16d43594592079d32a4d58ed31da21ef1b74f
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-03 09:27:52 -0300

    refactor: remove ssr

--- src/main.server.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from '#views/app/app.component';
-import { config } from '#views/app/app.config.server';
-
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
